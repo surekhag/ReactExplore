698e1e530f841b62d10164cb709352221c1dc049
surekhag <surekhag@github.com>
2019-11-13 11:11:00 +0530

    Assignment No 1

---
+++ assignments/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import LoginModule from './LoginModule/LoginModule'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">    
+    <LoginModule />
+    </div>
+  );
+}
+
+export default App;
