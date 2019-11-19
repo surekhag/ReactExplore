8b40dfad8493d8bec5a2c20d4bb4823bbab2c3af
surekhag <surekha.gadkari@objectedge.com>
2019-11-19 21:04:30 +0530

    Assignment No 1

---
+++ react-demo/src/Components/HomeModule/HomeModule.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+import LoginModule from '../LoginModule/LoginModule';
+
+class HomeModule extends Component {
+    render(){
+          return( <LoginModule />)    
+    }
+}
+export default HomeModule;
