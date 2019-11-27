3e13632b305b0b3b329e58006a8c683cc3f34c49
surekhag <surekha.gadkari@objectedge.com>
2019-11-27 22:59:18 +0530

    Added Loader

--- react-demo/src/Components/Loader/Loader.js
+++ react-demo/src/Components/Loader/Loader.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
-import Loader from 'react-loader-spinner'
+import Loader from 'react-loader-spinner';
  export default class LoaderModule extends React.Component {
   //other logic
     render() {
      return(
       <Loader
-         type="Puff"
-         color="#00BFFF"
+         type="oval"
+         color="#A9A9A9"
          height={100}
          width={100}
          timeout={3000} //3 secs
