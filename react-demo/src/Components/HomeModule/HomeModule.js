5590d4604b6a4ae44d7f3db6d31bbdd9c5261706
surekhag <surekha.gadkari@objectedge.com>
2019-12-01 19:54:09 +0530

    Fixed default route to login page

--- react-demo/src/Components/HomeModule/HomeModule.js
+++ react-demo/src/Components/HomeModule/HomeModule.js
@@ -2,13 +2,14 @@ import React from 'react';
 import LoginModule from '../LoginModule/LoginModule';
 import FeaturedProductsModule from '../FeaturedProductsModule/FeaturedProductsModule';
 import Navigation from '../Navigation/Navigation';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Redirect } from 'react-router-dom';
 
 const HomeModule = () => {
   return (
     <div>
       <BrowserRouter>
         <Navigation />
+        <Route exact path="/" component={() => <Redirect to="/login" />} />
         <Route path="/login" component={LoginModule} />
         <Route path="/featured products" component={FeaturedProductsModule} />
       </BrowserRouter>
