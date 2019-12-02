eeb960a8cdd1a7deb884849a05f5daefde389110
surekhag <surekha.gadkari@objectedge.com>
2019-12-02 09:08:42 +0530

    Eslint configurations updated

--- react-demo/lint-staged.config.js
+++ react-demo/lint-staged.config.js
@@ -1,10 +1,33 @@
 module.exports = {
-  linters: {
-    '**/*.+(js|md|ts|css|sass|less|graphql|yml|yaml|scss|json|vue)': [
-      'eslint --fix',
-      'prettier --write',
-      'jest --findRelatedTests',
-      'git add'
+  extends: [
+    '@oracle-commerce',
+    'plugin:import/errors',
+    'plugin:react/recommended',
+    'plugin:jsx-a11y/recommended',
+    'plugin:prettier/recommended',
+    'prettier/react'
+  ],
+  rules: {
+    'spellcheck/spell-checker': 0,
+    'react/prop-types': 0,
+    'no-console': 1,
+    'jsx-a11y/no-onchange': 0,
+    'sort-imports': 0,
+    'react/display-name': 0,
+    'prettier/prettier': [
+      'error',
+      {
+        singleQuote: true
+      }
     ]
+  },
+  plugins: ['react', 'import', 'jsx-a11y'],
+  parserOptions: {
+    ecmaVersion: 2019
+  },
+  env: {
+    browser: true,
+    jest: true,
+    node: true
   }
 };
